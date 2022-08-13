6f2a3eaa0bcf7e93eb547af7228ece86ce3ac2ef
Yakov Kolyada <yakov.kolyada@gmail.com>
2022-08-13 17:59:29 +0300

    scripts v0.1

--- 04BashEssentials/0403Scripts/scripts.sh
+++ 04BashEssentials/0403Scripts/scripts.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 # scripts.sh
 #
+dir_name="Name_Surname"
 cd ~/homework
 echo "#!`which bash`" > ./script.sh
+chmod a+x ./script.sh
+echo "echo \"Creating folder\"" >> ./script.sh
+cat << EOF
+mkdir $dir_name
+ls -l | grep "^d"
+echo "echo \"Creating files\""
+touch ./Name
+touch ./Surname
+ls $dir_name
+EOF >> ./script.sh
+
+bash script.sh
