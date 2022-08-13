f19f0688f479d47fd185c51c7d52bea680c0acb6
Yakov Kolyada <yakov.kolyada@gmail.com>
2022-08-13 20:19:40 +0300

    variables v0.2

--- 04BashEssentials/0404Variables/variables.sh
+++ 04BashEssentials/0404Variables/variables.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 # variables.sh
 #
-echo "#!`which bash`" > ./$script_name
+NAME="Name_Surname"
+cd ~
 
-dir_name="Name_Surname"
-script_name=script2.sh
-
-if [ -z ./homework]
-    mkdir homework
-else
+if [ -d "./homework/" ]; then
     cd ~/homework
+else
+    mkdir homework
 fi
 
-echo "#!`which bash`" > ./$script_name
-chmod a+x ./script.sh
-echo "echo \"Creating folder\"" >> ./$script_name
-echo "mkdir $dir_name
-ls -l | grep ""^d""
-echo ""Creating files""
-touch ./Name
-touch ./Surname
-ls $dir_name" >> $script_name 
+echo "FILE=""new_file"" > ./vars
+source ./vars
 
-bash $script_name
+mkdir $NAME
+touch ./$NAME/$FILE
+ls -R .
