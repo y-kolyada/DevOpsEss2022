a1ccf83dc0213cde70a9cd17709b19aec15f9e1c
Yakov Kolyada <yakov.kolyada@gmail.com>
2022-08-13 18:35:04 +0300

    scripts2 v0.3

--- 04BashEssentials/0403Scripts/scripts2.sh
+++ 04BashEssentials/0403Scripts/scripts2.sh
@@ -7,7 +7,7 @@ cd ~/homework
 echo "#!`which bash`" > ./$script_name
 chmod a+x ./script.sh
 echo "echo \"Creating folder\"" >> ./$script_name
-cat >> ./$script_name << EOF
+cat >> ./$script_name <<EOF
 mkdir $dir_name
 ls -l | grep "^d"
 echo "Creating files"
