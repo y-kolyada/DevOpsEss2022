06c871c8b6e418c1b6edeeb4c17fb13817510bdc
Yakov Kolyada <yakov.kolyada@gmail.com>
2022-08-15 21:45:31 +0300

    pos_args v0.1

---
+++ 04BashEssentials/0414PosArgs/pos_args.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# pos_args.sh
+#
+DIR="./homework"
+cd ~
+
+if [ -d $DIR ]; then
+    cd $DIR
+    echo "cd into $DIR..."
+else
+    mkdir $DIR
+    echo "mkdir $DIR..."
+    cd $DIR
+fi
+
+i=1
+
+for n in ${@}; do
+  echo "Arg$i: $n"
+  arr[i]=$n
+  (( i++ ))
+done
+
+echo ""
+i=1
+
+for n in ${@}; do
+  if [ $i -eq $# ]; then
+    (( res=arr[i]+arr[1] ))
+  else
+    (( res=arr[i]+arr[i+1] ))
+  fi
+  echo -n "$res "
+  (( i++ ))
+done
+
+echo ""
