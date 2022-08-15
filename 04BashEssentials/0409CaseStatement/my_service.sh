4df6e76ebabb6f494d38965087066d103592b8e1
Yakov Kolyada <yakov.kolyada@gmail.com>
2022-08-15 09:52:38 +0300

    my_service v0.1

---
+++ 04BashEssentials/0409CaseStatement/my_service.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# my_service.sh
+#
+DIR="./homework"
+cd ~
+
+if [ -d $DIR ]; then
+    cd $DIR
+    echo "cd into $DIR..."
+else
+    mkdir $DIR
+    echo "mkdir $DIR..."
+    cd $DIR
+fi
+
+case $1 in
+  start)
+    echo "Service started"
+    sleep 9999
+    ;;
+  stop)
+    PROC=$$
+    echo $PROC
+    ps -f $PROC
+    echo "Service stopped"
+    ;;
+  restart)
+    .my_service.sh stop
+    .my_service.sh start
+    ;;
+  {*,help}
+    echo "
+    - add your service to PATH
+    - run your script with start and restart arguments in background
+    - run your script with stop and help arguments"
+    ;;
+esac
