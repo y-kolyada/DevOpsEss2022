12de4703991dc9007509a40b26bbb7ccfda17f65
Yakov Kolyada <yakov.kolyada@gmail.com>
2022-08-15 09:24:02 +0300

    odd_even v0.6

--- 04BashEssentials/0408IfStatements/odd_even.sh
+++ 04BashEssentials/0408IfStatements/odd_even.sh
@@ -33,12 +33,12 @@ chmod a+x ./my_script.sh
 tee -a my_script.sh << EOF
 LAST=\$_
 
-if [ $# -lt 2 ]; then
-  echo $1
-elif [ $# -gt 2 ] && [ $# -lt 4 ]; then
+if [ \$# -lt 2 ]; then
+  echo \$1
+elif [ \$# -gt 2 ] && [ \$# -lt 4 ]; then
   echo \${@:\$#}
-  echo \${*: -1}  
-  echo \${!#}
+  # echo \${*: -1}  
+  # echo \${!#}
 else
   echo "Invalid number of arguments"
 fi
